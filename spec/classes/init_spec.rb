2ba61794740a5aa55dbd5ed1d117be8dcfb71444
unknown <bstopp@bstopp2.VML.LAN>
2015-04-16 16:28:55 -0500

    additional tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -3,6 +3,16 @@ describe 'adobe_experience_manager' do
 
   context 'with defaults for all parameters' do
     it { is_expected.to contain_class('adobe_experience_manager') }
+    it { is_expected.to contain_group('aem').with(
+      ensure => 'present',
+      )
+    }
+    it { is_expected.to contain_user('aem').with(
+      ensure  => 'present',
+      gid     => 'aem',
+      )
+    }
+
     it { is_expected.to contain_file('/opt/aem').with(
       'ensure' => 'directory',
       'owner' => 'aem',
