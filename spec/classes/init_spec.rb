91812df0b433f60f89d2d22db5902ffddb86ea38
unknown <bstopp@bstopp2.VML.LAN>
2015-04-26 22:05:31 -0500

    Adding dependency tree to test cases.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -34,8 +34,10 @@ describe 'adobe_experience_manager' do
       )
     }
     it { is_expected.to contain_class('adobe_experience_manager::user') }
-    it { is_expected.to contain_class('adobe_experience_manager::config') }
-    it { is_expected.to contain_class('adobe_experience_manager::install') }
+    it { is_expected.to contain_class('adobe_experience_manager::config').that_requires('adobe_experience_manager::user') }
+    it { is_expected.to contain_class('adobe_experience_manager::config').that_notifies('adobe_experience_manager::service') }
+    it { is_expected.to contain_class('adobe_experience_manager::install').that_requires('adobe_experience_manager::config') }
+    it { is_expected.to contain_class('adobe_experience_manager::install').that_notifies('adobe_experience_manager::service') }
     it { is_expected.to contain_class('adobe_experience_manager::service') }
 
   end
