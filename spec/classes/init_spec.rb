2b0d5e197bdc64dfab28b859f25ad71c785f4025
unknown <bstopp@bstopp2.VML.LAN>
2015-05-08 14:21:09 -0500

    Even more parameter validation.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -60,6 +60,147 @@ describe 'adobe_experience_manager' do
     it { is_expected.to contain_class('adobe_experience_manager').with({ :includesamples => false}) }
   end
 
+  context 'defaults for all parameters; invalid port' do
+    let :params do 
+      {
+        :jar              => '/opt/aem/cq-author-4502.jar',
+        :version          => '6.0',
+        :port             => 'foo',
+      }
+    end
+    
+    it do
+      expect {
+        catalogue
+      }.to raise_error(Puppet::Error, /not an integer/)
+    end
+  end
+
+  context 'defaults for all parameters; port 4502' do
+    let :params do 
+      {
+        :jar              => '/opt/aem/cq-author-4502.jar',
+        :version          => '6.0',
+        :port             => 4502,
+      }
+    end
+    
+    it { is_expected.to contain_class('adobe_experience_manager').with({ :port => 4502}) }
+  end
+
+  context 'defaults for all parameters; invalid log_level' do
+    let :params do 
+      {
+        :jar              => '/opt/aem/cq-author-4502.jar',
+        :version          => '6.0',
+        :log_level        => 'foo',
+      }
+    end
+    
+    it do
+      expect {
+        catalogue
+      }.to raise_error(Puppet::Error, /not an integer/)
+    end
+  end
+
+  context 'defaults for all parameters; log_level 3' do
+    let :params do 
+      {
+        :jar              => '/opt/aem/cq-author-4502.jar',
+        :version          => '6.0',
+        :log_level        => 3,
+      }
+    end
+    
+    it { is_expected.to contain_class('adobe_experience_manager').with({ :log_level => 3}) }
+  end
+
+  context 'defaults for all parameters; invalid jvm_opts' do
+    let :params do 
+      {
+        :jar              => '/opt/aem/cq-author-4502.jar',
+        :version          => '6.0',
+        :jvm_opts         => false,
+      }
+    end
+    
+    it do
+      expect {
+        catalogue
+      }.to raise_error(Puppet::Error, /not a string/)
+    end
+  end
+
+  context 'defaults for all parameters; jvm_opts specified' do
+    let :params do 
+      {
+        :jar              => '/opt/aem/cq-author-4502.jar',
+        :version          => '6.0',
+        :jvm_opts         => '-Xmx2048m',
+      }
+    end
+    
+    it { is_expected.to contain_class('adobe_experience_manager').with({ :jvm_opts => '-Xmx2048m'}) }
+  end
+
+  context 'defaults for all parameters; invalid mongo' do
+    let :params do 
+      {
+        :jar              => '/opt/aem/cq-author-4502.jar',
+        :version          => '6.0',
+        :mongo            => 'foo',
+      }
+    end
+    
+    it do
+      expect {
+        catalogue
+      }.to raise_error(Puppet::Error, /not a boolean/)
+    end
+  end
+
+  context 'defaults for all parameters; mongo true' do
+    let :params do 
+      {
+        :jar              => '/opt/aem/cq-author-4502.jar',
+        :version          => '6.0',
+        :mongo            => true,
+      }
+    end
+    
+    it { is_expected.to contain_class('adobe_experience_manager').with({ :mongo => true}) }
+  end
+
+    context 'defaults for all parameters; invalid jvm_opts' do
+    let :params do 
+      {
+        :jar              => '/opt/aem/cq-author-4502.jar',
+        :version          => '6.0',
+        :mongo_uri        => false,
+      }
+    end
+    
+    it do
+      expect {
+        catalogue
+      }.to raise_error(Puppet::Error, /not a string/)
+    end
+  end
+
+  context 'defaults for all parameters; jvm_opts specified' do
+    let :params do 
+      {
+        :jar              => '/opt/aem/cq-author-4502.jar',
+        :version          => '6.0',
+        :mongo_uri         => 'mongodb://127.0.0.1:27017',
+      }
+    end
+    
+    it { is_expected.to contain_class('adobe_experience_manager').with({ :mongo_uri => 'mongodb://127.0.0.1:27017a'}) }
+  end
+
+
   context 'required jar and version and defaults for all other parameters' do
     let :params do
       {
