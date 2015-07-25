f275d76b044e7f92cf6850d3e266f3ffe911daa9
Bryan Stopp <bryan.stopp@gmail.com>
2015-07-25 13:58:54 -0500

    Moving to codeclimate from Coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,10 @@
 require 'rubygems'
 require 'rspec/mocks'
 require 'puppetlabs_spec_helper/module_spec_helper'
-require 'coveralls'
 require "codeclimate-test-reporter"
 
 
 CodeClimate::TestReporter.start
-Coveralls.wear!
 
 RSpec.configure do |config|
   config.mock_with :rspec
