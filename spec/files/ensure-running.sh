8f48767d6e35a48ace22739a0a0a3f930e6d42aa
Bryan Stopp <bryan.stopp@gmail.com>
2016-08-27 22:00:44 -0500

    Replacing missing script.

--- spec/files/ensure-running.sh
+++ spec/files/ensure-running.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+
+idx=0
+while [  $idx -lt 60 ]; do
+  resp_code=`curl -s -o /dev/null -w "%{http_code}" http://localhost:4502`
+  if [[ $? -eq 0 ]] && [[ $resp_code =~ (302|401|200) ]]; then
+    exit 0
+  fi
+ sleep 10
+ let idx=idx+1
+done
+
